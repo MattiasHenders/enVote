89334dcd9e6c83040ef0e5422088942f6529dde6
MattiasHenders <mhenders1@my.bcit.ca>
2021-10-02 15:17:31 -0700

    Maps working. Dynamic Email generation

--- app/src/main/java/com/envelopepushers/envote/LocationSelectMap.java
+++ app/src/main/java/com/envelopepushers/envote/LocationSelectMap.java
@@ -1,36 +1,93 @@
 package com.envelopepushers.envote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
 
 public class LocationSelectMap extends AppCompatActivity {
 
     private MapView mapMain;
-    private MapController mapController;
+    private IMapController mapController;
+    private Button btnSubmitLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_select_map);
 
-        mapMain = (MapView) findViewById(R.id.map_main);
-        mapMain.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-        mapMain.setBuiltInZoomControls(true);
-        mapController = (MapController) mapMain.getController();
-        mapController.setZoom(13);
-        GeoPoint gPt = new GeoPoint(51500000, -150000);
-        mapController.setCenter(gPt);
+        //Check permissions
+        checkPermissions();
+
+        //load/initialize the osmdroid configuration, this can be done
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        //Set the map objects
+        mapMain = findViewById(R.id.map_main);
+        mapMain.setTileSource(TileSourceFactory.MAPNIK);
+
+        mapController = mapMain.getController();
+        mapController.setZoom(17);
+
+        //Burnaby lat/lon as default
+        GeoPoint startPoint = new GeoPoint(49.2488, -122.9805);
+        mapController.setCenter(startPoint);
+
+        btnSubmitLocation = findViewById(R.id.btn_location_submit);
+
+        //OnClickListeners Set
+        btnSubmitLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void onResume(){
+        super.onResume();
+        mapMain.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+        mapMain.onPause();
+    }
+
+    /**
+     * Checks the phones permissions for storage writing.
+     * Needed for MapView
+     */
+    private void checkPermissions() {
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Log.v("PERMS", "Permission is granted");
+        } else {
+            Log.v("PERMS", "Permission is revoked");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
     }
 }
--- app/src/main/java/com/envelopepushers/envote/TemplateView.java
+++ app/src/main/java/com/envelopepushers/envote/TemplateView.java
@@ -3,12 +3,50 @@ package com.envelopepushers.envote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TemplateView extends AppCompatActivity {
+
+    public TextView textEmailTo;
+    public TextView textSubject;
+    public TextView textBody;
+
+    public Button btnSendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template_view);
+
+        //TextView setting objects
+        textEmailTo = findViewById(R.id.text_email_to_line);
+        textSubject = findViewById(R.id.text_email_subject);
+        textBody = findViewById(R.id.text_email_body);
+
+        setEmailTemplate();
+
+        btnSendEmail = findViewById(R.id.btn_send_email);
+
+
+        //OnClickListeners Set
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Sets the content in the email template.
+     */
+    private void setEmailTemplate() {
+
+        textEmailTo.setText(getString(R.string.email_to) + " PM Mattias");
+        textSubject.setText(getString(R.string.email_subject) + " Fix the Water!");
+        textBody.setText(getString(R.string.filler_text));
+
     }
 }
