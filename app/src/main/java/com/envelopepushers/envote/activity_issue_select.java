9db236225c7b5b46a4cd55f04bfe11fa14eda812
Chris Spooner <Christopher.Spooner97@gmail.com>
2021-10-03 15:43:42 -0700

    Add expandable text fields to issues page

--- app/src/main/java/com/envelopepushers/envote/activity_issue_select.java
+++ app/src/main/java/com/envelopepushers/envote/activity_issue_select.java
@@ -7,12 +7,15 @@ import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class activity_issue_select extends AppCompatActivity {
 
     private Button btnSubmitLocation;
-
-
+    TextView textView1;
+    Button showMore1;
+    TextView textView2;
+    Button showMore2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,44 @@ public class activity_issue_select extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openActivity();
+            }
+        });
+
+        textView1=(TextView)findViewById(R.id.textView1);
+        showMore1=(Button)findViewById(R.id.showMore1);
+        showMore1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (showMore1.getText().toString().equalsIgnoreCase("Showmore..."))
+                {
+                    textView1.setMaxLines(Integer.MAX_VALUE);//your TextView
+                    showMore1.setText("Showless");
+                }
+                else
+                {
+                    textView1.setMaxLines(3);//your TextView
+                    showMore1.setText("Showmore...");
+                }
+            }
+        });
+
+        textView2=(TextView)findViewById(R.id.textView2);
+        showMore2=(Button)findViewById(R.id.showMore2);
+        showMore2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (showMore2.getText().toString().equalsIgnoreCase("Showmore..."))
+                {
+                    textView2.setMaxLines(Integer.MAX_VALUE);//your TextView
+                    showMore2.setText("Showless");
+                }
+                else
+                {
+                    textView2.setMaxLines(3);//your TextView
+                    showMore2.setText("Showmore...");
+                }
             }
         });
     }
