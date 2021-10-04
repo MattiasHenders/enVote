be2b4265f4dc8c3b4536db7dfc8084a91c311fdc
MattiasHenders <mhenders1@my.bcit.ca>
2021-10-04 09:46:21 -0700

    Dynamic cards and menu

---
+++ app/src/main/java/com/envelopepushers/envote/HomeActivity.java
@@ -0,0 +1,132 @@
+package com.envelopepushers.envote;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HomeActivity extends AppCompatActivity {
+
+    LinearLayout pastEmailsLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        pastEmailsLayout = findViewById(R.id.container_past_emails);
+
+        generagePastEmailCards();
+    }
+
+    private void generagePastEmailCards() {
+
+        //Create the individual card
+        CardView pastEmailCard = new CardView(this);
+
+        //Set the Card params
+        CardView.LayoutParams cardLayoutParams = new CardView.LayoutParams(
+                CardView.LayoutParams.MATCH_PARENT,
+                CardView.LayoutParams.MATCH_PARENT);
+        cardLayoutParams.leftMargin = 50;
+        pastEmailCard.setRadius(
+                (int)getResources().getDimension(R.dimen.borderRadius_medium));
+
+        pastEmailCard.setLayoutParams(cardLayoutParams);
+
+        LinearLayout cardHolderLayout = new LinearLayout(this);
+        cardHolderLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams cardHolderLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        cardHolderLayout.setPadding(
+                (int)getResources().getDimension(R.dimen.padding_medium),
+                (int)getResources().getDimension(R.dimen.padding_medium),
+                (int)getResources().getDimension(R.dimen.padding_medium),
+                (int)getResources().getDimension(R.dimen.padding_medium));
+
+        cardHolderLayout.setBackgroundColor(getColor(R.color.dull_grey));
+        cardHolderLayout.setLayoutParams(cardHolderLayoutParams);
+
+        //Set the Header Layout params and layout
+        LinearLayout headerLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams headerLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.FILL_PARENT);
+        headerLayout.setOrientation(LinearLayout.HORIZONTAL);
+        headerLayout.setLayoutParams(headerLayoutParams);
+
+        //Set the Header Text layout and params
+        LinearLayout headerTextLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams headerTextLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        headerTextLayout.setOrientation(LinearLayout.VERTICAL);
+        headerTextLayout.setLayoutParams(headerTextLayoutParams);
+
+        //Set the email TO name as the title
+        TextView cardTitle = new TextView(this);
+        cardTitle.setText(getString(R.string.home_test_name));
+        //cardTitle.setTextSize(getResources().getDimension(R.dimen.font_small));
+        cardTitle.setTextSize(24);
+        cardTitle.setTextColor(getColor(R.color.dark_grey));
+
+        //Set the sub-header as the date the email was sent
+        TextView cardSubHeader = new TextView(this);
+        SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd yyyy");
+        cardSubHeader.setText(formatter.format(new Date()));
+        //cardSubHeader.setTextSize(getResources().getDimension(R.dimen.font_small));
+        cardSubHeader.setTextSize(22);
+        cardSubHeader.setTextColor(getColor(R.color.dark_grey));
+
+        //Set the body as the first bit of the email
+        TextView cardBody = new TextView(this);
+        cardBody.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        cardBody.setText(getString(R.string.home_test_description));
+        //cardBody.setTextSize(getResources().getDimension(R.dimen.font_small));
+        cardBody.setTextSize(14);
+        cardBody.setTextColor(getColor(R.color.dark_grey));
+
+        //Set the image as the icon for the issue
+        ImageView cardIcon = new ImageView(this);
+        LinearLayout.LayoutParams cardIconLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        cardIconLayoutParams.setMarginStart((int)getResources().getDimension(R.dimen.margin_xxxLarge));
+        cardIcon.setLayoutParams(cardIconLayoutParams);
+        cardIcon.setImageIcon(Icon.createWithResource(this, R.drawable.ic_baseline_eco_24));
+        cardIcon.setMinimumHeight((int)getResources().getDimension(R.dimen.imgSize_small));
+        cardIcon.setMinimumWidth((int)getResources().getDimension(R.dimen.imgSize_small));
+        cardIcon.setImageTintList(ColorStateList.valueOf(getColor(R.color.dark_grey)));
+
+        //Add items in reverse order to the layout holder
+        headerTextLayout.addView(cardTitle);
+        headerTextLayout.addView(cardSubHeader);
+        headerLayout.addView(headerTextLayout);
+        headerLayout.addView(cardIcon);
+        cardHolderLayout.addView(headerLayout);
+        cardHolderLayout.addView(cardBody);
+
+        //Add the filled Linear Layout to the full card
+        pastEmailCard.addView(cardHolderLayout);
+
+        //Add card to the layout that holds the past trips
+        LinearLayout view = findViewById(R.id.container_past_emails);
+        view.addView(pastEmailCard, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+    }
+}
--- app/src/main/java/com/envelopepushers/envote/MainActivity.java
+++ app/src/main/java/com/envelopepushers/envote/MainActivity.java
@@ -17,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         Intent i2 = new Intent(this, LocationSelectMap.class);
         startActivity(i2);
+
+        Intent i3 = new Intent(this, HomeActivity.class);
+        startActivity(i3);
     }
 }
