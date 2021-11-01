f7134603d7435257d483603706c86fad8c3e1bf9
Bryan Xing <bxing@my.bcit.ca>
2021-10-31 22:19:05 -0700

    trying firebaseAuth login

--- app/src/main/java/com/envelopepushers/envote/LoginActivity.java
+++ app/src/main/java/com/envelopepushers/envote/LoginActivity.java
@@ -1,11 +1,12 @@
 package com.envelopepushers.envote;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,10 +22,20 @@ public class LoginActivity extends AppCompatActivity {
                 openHomeActivity();
             }
         });
+        TextView signUp = findViewById(R.id.signupLink);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openSignUpActivity();}
+        });
     }
 
     private void openHomeActivity() {
         startActivity(new Intent(this, HomeActivity.class));
+        finish();
+    }
+
+    private void openSignUpActivity() {
+        startActivity(new Intent(this, SignUpActivity.class));
         finish();
     }
 }
--- app/src/main/java/com/envelopepushers/envote/SignUpActivity.java
+++ app/src/main/java/com/envelopepushers/envote/SignUpActivity.java
@@ -1,14 +1,97 @@
 package com.envelopepushers.envote;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+    FirebaseAuth fAuth;
+    EditText editEmail, editPassword;
+    Button btnRegister;
+    TextView signUp;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        btnRegister = findViewById(R.id.registerBtn);
+        progressBar = findViewById(R.id.progressBar);
+        signUp = findViewById(R.id.signinLink);
+
+        fAuth = FirebaseAuth.getInstance();
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            finish();
+        }
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    editEmail.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    editEmail.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    editEmail.setError("Password must be >= 6 characters.");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // register user
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "User Created", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "ERROR: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignInActivity();
+            }
+        });
+    }
+
+    private void openSignInActivity() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
