66a7a33830fdf89ec9d71d4de46885608280132b
Chris Spooner <Christopher.Spooner97@gmail.com>
2021-11-26 16:45:52 -0800

    Comment Eco Issues

--- app/src/main/java/com/envelopepushers/envote/EcoIssues.java
+++ app/src/main/java/com/envelopepushers/envote/EcoIssues.java
@@ -19,15 +19,45 @@ public enum EcoIssues {
     AIR("AIR", "Air Quality", R.drawable.ic_baseline_air_24,
              R.color.teal_bright, R.color.teal_dark, R.string.issue_air);// Resources.getSystem().getString(R.string.issue_air));
 
-
+    /**
+     * Key to reference the issue type.
+     */
     private final String key;
+
+    /**
+     * AThe name of the eco issue.
+     */
     private final String name;
+
+    /**
+     * The icon of the eco issue.
+     */
     private final int icon;
+
+    /**
+     * The primary color of the eco issue.
+     */
     private final int colourLight;
+
+    /**
+     * The secondary color of the eco issue.
+     */
     private final int colourDark;
+
+    /**
+     * The description of the eco issue.
+     */
     private final int description;
 
-
+    /**
+     * Eco Issue constructor
+     * @param key String
+     * @param name String
+     * @param icon int
+     * @param colourLight int
+     * @param colourDark int
+     * @param description int
+     */
   EcoIssues(String key, String name, int icon, int colourLight, int colourDark, int description) {
     this.key = key;
     this.name = name;
@@ -37,27 +67,51 @@ public enum EcoIssues {
     this.description = description;
   }
 
+    /**
+     * Gets the key.
+     * @return key
+     */
   public String getKey() {
     return key;
   }
 
+    /**
+     * Gets the name.
+     * @return name
+     */
   public String getName() {
     return name;
   }
 
+    /**
+     * Gets the icon.
+     * @return icon
+     */
   public int getIcon() {
     return icon;
   }
 
+    /**
+     * Gets the primary color.
+     * @return color
+     */
   public int getColourLight() {
     return colourLight;
   }
+
+    /**
+     * Gets the secondary color.
+     * @return color
+     */
   public int getColourDark() {
     return colourDark;
   }
 
+    /**
+     * Gets the description.
+     * @return description
+     */
   public int getDescription() {
       return description;
   }
-
 }
